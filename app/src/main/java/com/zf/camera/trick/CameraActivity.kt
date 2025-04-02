bfaf4684a92e3e8f6eb1ae0bf47f731da36e0847
Little <414929680@qq.com>
2025-04-02 10:21:15 +0800

    [Add] init commit

---
+++ app/src/main/java/com/zf/camera/trick/CameraActivity.kt
@@ -0,0 +1,68 @@
+package com.zf.camera.trick
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import pub.devrel.easypermissions.EasyPermissions
+
+
+class CameraActivity: ComponentActivity(), EasyPermissions.RationaleCallbacks, EasyPermissions.PermissionCallbacks {
+
+    companion object {
+        fun startActivity(activity: Activity) {
+            activity.startActivity(Intent(activity, CameraActivity::class.java))
+        }
+
+        val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        startCamera()
+    }
+
+    private fun startCamera() {
+        if (EasyPermissions.hasPermissions(this,  *permissions)) {
+            startPreview()
+        } else {
+            EasyPermissions.requestPermissions(this, "该权限仅是实现拍摄音视频，请通过权限否则不能使用某些功能！", 100, *permissions)
+        }
+    }
+
+    private fun startPreview() {
+
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+        Toast.makeText(this, "您已同意权限申明", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        Toast.makeText(this, "您已拒绝权限申明", Toast.LENGTH_SHORT).show()
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, permissions.toList())) {
+            //在权限弹窗中，用户勾选了'不在提示'且拒绝权限的情况触发
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        //start preview
+        startPreview()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        Toast.makeText(this, "您已拒绝权限", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+}
---
+++ app/src/main/java/com/zf/camera/trick/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.zf.camera.trick
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zf.camera.trick.ui.theme.MainActivityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<AppCompatTextView>(R.id.camera).setOnClickListener {
+            CameraActivity.startActivity(this)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainActivityTheme {
+        Greeting("Android")
+    }
+}
