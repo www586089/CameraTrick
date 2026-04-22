9c2c239a95b981820462d5aa51019447a691a5d1
Little <414929680@qq.com>
2026-04-22 13:58:30 +0800

    [ADD] APK打包配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,8 @@
+import java.io.FileInputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -10,6 +15,44 @@ android {
     viewBinding {
         enable = true
     }
+
+    // 加载签名配置文件（正式项目必用）
+    val keystorePropertiesFile = rootProject.file("./key/keystore.properties")
+    System.out.println("keystorePropertiesFile : ${keystorePropertiesFile.absolutePath}")
+    val keystoreProperties = Properties()
+
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+        System.out.println(file(keystoreProperties["storeFile"] as String))
+    } else {
+        System.out.println("keystorePropertiesFile not found ")
+    }
+// ==============================================
+    // 签名配置（核心部分）
+    // ==============================================
+    signingConfigs {
+        create("release") {
+            // 从配置文件读取
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+
+            // 签名版本（兼容所有设备）
+            isV1SigningEnabled = true
+            isV2SigningEnabled = true
+//            isV3SigningEnabled = true
+        }
+
+        // 可选：调试签名
+//        create("debug") {
+//            storeFile = file("debug.keystore")
+//            storePassword = "android"
+//            keyAlias = "androiddebugkey"
+//            keyPassword = "android"
+//        }
+    }
+
 
     defaultConfig {
         applicationId = "com.zf.camera.trick"
@@ -30,17 +73,54 @@ android {
         }
     }
 
+    // ======================================================
+    // 【核心】自定义 APK 输出名称 + 输出到 根目录/pkg
+    // ======================================================
+    applicationVariants.configureEach {
+        val variant = this
+
+        if (variant.buildType.name == "release") {
+            assembleProvider.configure {
+                doLast {
+                    // 时间戳
+                    val dateFormat = SimpleDateFormat("yyyy-MMddHHmm")
+                    val buildTime = dateFormat.format(Date())
+
+                    // 输出目录
+                    val outDir = File(rootProject.projectDir, "pkg")
+                    outDir.mkdirs()
+
+                    // 复制 APK
+                    val apkInput = File("${layout.buildDirectory.get()}/outputs/apk/release/app-release.apk")
+
+                    if (apkInput.exists()) {
+                        val apkOutput = File(outDir, "CameraTrick_V${variant.versionName}_${buildTime}_Release.apk")
+                        apkInput.copyTo(apkOutput, overwrite = true)
+                    }
+
+                    // 复制 mapping
+                    val mappingInput = File("${layout.buildDirectory.get()}/outputs/mapping/release/mapping.txt")
+                    if (mappingInput.exists()) {
+                        val mappingOutput = File(outDir, "mapping.txt")
+                        mappingInput.copyTo(mappingOutput, overwrite = true)
+                    }
+                }
+            }
+        }
+    }
+
     sourceSets["main"].jniLibs {
         srcDir("./libs")
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
@@ -59,6 +139,18 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+// ✅ clean 时自动删除 pkg 目录下的文件
+tasks.clean {
+    doLast {
+        val pkgDir = File(rootProject.projectDir, "pkg")
+        if (pkgDir.exists()) {
+            pkgDir.listFiles()?.forEach { file ->
+                file.deleteRecursively()
+            }
         }
     }
 }
